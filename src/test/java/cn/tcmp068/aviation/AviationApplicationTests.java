b60e8571bc6f1c3860959750a9537d8317bdf874
yangxuefang <yangxuefang34@gmail.com>
2019-01-05 16:01:06 +0800

    推送

---
+++ src/test/java/cn/tcmp068/aviation/AviationApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.tcmp068.aviation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AviationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
