22010119de4437f023d8525f8d8e1f9819bd33b4
yangxuefang <yangxuefang34@gmail.com>
2019-01-10 15:09:20 +0800

    1

--- src/main/java/cn/tcmp068/aviation/entity/Admin.java
+++ src/main/java/cn/tcmp068/aviation/entity/Admin.java
@@ -4,16 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
 import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Admin {
+
     private int adminId;
     private String adminPhone;
     private String adminName;
-    private Date adminJoinDate;
+    private Timestamp adminTime;
     private String adminPassword;
-    private int adminLevel;
+    private int adminGrade;
     private int adminStatus;
 }
