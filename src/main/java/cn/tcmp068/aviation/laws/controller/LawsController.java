cf7529904ead3392e0b0e0ec1618e2e43afcd3c5
wy <1150471222@qq.com>
2019-01-17 16:17:17 +0800

    111

--- src/main/java/cn/tcmp068/aviation/laws/controller/LawsController.java
+++ src/main/java/cn/tcmp068/aviation/laws/controller/LawsController.java
@@ -1,5 +1,6 @@
 package cn.tcmp068.aviation.laws.controller;
 
+import cn.tcmp068.aviation.entity.Laws;
 import cn.tcmp068.aviation.laws.service.LawsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,7 +14,8 @@ public class LawsController {
     private LawsService lawsService;
 
     @RequestMapping("queryAllLaws")
-    public String queryAllLaws(Model model){
-        return "";
+    public String queryAllLaws(Model model, Laws laws,int PageNumber,int PageSize){
+        model.addAttribute("laws",this.lawsService.queryAll(laws,PageNumber,PageSize));
+        return "lawsList";
     }
 }
